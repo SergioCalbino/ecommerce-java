4f6502a55983345cef6bd97ce868ee40fe522294
scalbino <sergiocalbino@it.sportclub.com.ar>
2025-10-05 09:37:42 -0300

    Se agrega el buscador a la lista

--- src/main/java/com/example/demo/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -2,13 +2,16 @@ package com.example.demo.repositories;
 
 import com.example.demo.entities.Product;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     boolean existsByNameIgnoreCase(String name);
     Optional<Product> findByNameIgnoreCase (String name);
+    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
 }
